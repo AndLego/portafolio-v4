d38241f2bd98159bc14369b6a448693dc17a25ea
AndLego <galeon.gb@gmail.com>
2023-07-21 13:05:56 -0500

    connected emailjs to form and installed three js dependencies

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import style from "./Contact.module.css"
 import Map from "../Map/Map.jsx"
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const [success, setSuccess] = React.useState(false)
+    const ref = React.useRef<HTMLFormElement>(null)
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (ref.current) {
+            emailjs.sendForm('service_mbuj46q', 'template_9dpo8sa', ref.current, '5hrYs6EDaOtWnu2XP')
+                .then((result) => {
+                    console.log(result.text);
+                    setSuccess(true)
+                }, (error) => {
+                    console.log(error.text);
+                    setSuccess(false)
+                });
+        }
+    }
+
     return (
         <section className={style.contact}>
             <article className={style.left}>
-                <form action="">
+                <form ref={ref} onSubmit={handleSubmit}>
                     <h2>Contact Me</h2>
-                    <input placeholder='Name' type="text" />
-                    <input placeholder='Email' type="text" />
-                    <textarea placeholder='Write your message' name="" id="" rows={10}></textarea>
-                    <button>Send</button>
+                    <input placeholder='Name' type="text" name="name" />
+                    <input placeholder='Email' type="text" name="email" />
+                    <textarea placeholder='Write your message' name="message" id="" rows={10}></textarea>
+                    <button type='submit'>Send</button>
+                    {success &&
+                        "Your message has been sent. I'll get back to you soon :)"
+                    }
                 </form>
             </article>
             <article className={style.right}>
--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,8 @@
-import React from "react";
 import {
     ComposableMap,
     Geographies,
     Geography,
     Annotation,
-    ZoomableGroup
 } from "react-simple-maps";
 import style from "./Map.module.css"
 
